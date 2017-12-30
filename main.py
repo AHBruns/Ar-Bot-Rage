e2eebadb9ddb4686f19fdbeb528e2e1c7824e6d3
AHBruns <alex.h.bruns@gmail.com>
2017-12-30 13:28:00 -0600

    added an event loop check to update market info

--- main.py
+++ main.py
@@ -11,14 +11,21 @@ completed_event_loop_count = 0
 while True:
     # base currencies check
     #    - first time through
-    #    - every 10 loops
-    #    - every 150 seconds
-    if completed_event_loop_count == 0 or completed_event_loop_count % 10 == 0 or time.time() - last_check_time_base_currencies > 150:
+    #    - every 150 seconds max
+    if completed_event_loop_count == 0 or time.time() - last_check_time_base_currencies > 150:
         base_currencies = phone.get_base_currency_ids()
         last_check_time_base_currencies = time.time()
         print('\tbase currencies checked for activity and new additions')
-        print('\t' + str(base_currencies))
         print('\ttime: ' + str(last_check_time_base_currencies))
+        print('')
+    # markets check
+    #    - first time through
+    #    - every 5 seconds max
+    if completed_event_loop_count == 0 or time.time() - last_check_time_markets > 5:
+        markets = phone.get_market_infos()
+        last_check_time_markets = time.time()
+        print('\tmarkets checked for new prices, activity, and new additions')
+        print('\ttime: ' + str(last_check_time_markets))
         print('')
 
     completed_event_loop_count += 1
